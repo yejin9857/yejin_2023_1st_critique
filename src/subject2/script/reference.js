c5d837ed5f6d21546e38fc6d6aa9282083afb7a9
yejin9857 <134900927+yejin9857@users.noreply.github.com>
2023-09-05 04:36:28 +0900

    1차심사_출처표기수정

--- src/subject2/script/reference.js
+++ src/subject2/script/reference.js
@@ -6,4 +6,11 @@ const reference = [
     sourceTitle: "",
     link: "https://www.ted.com/talks/ted_ed_the_4_greatest_threats_to_the_survival_of_humanity?language=ko",
   },
+  {
+    author: "C. 티 응우옌, 이동휘 옮김",
+    title: "게임 : 행위성의 예술",
+    year: "2022",
+    sourceTitle: "워크룸 프레스",
+    link: "",
+  },
 ];
