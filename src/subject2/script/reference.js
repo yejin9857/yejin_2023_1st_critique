e50c9f128e908284f1eedc94b5ea311e10d90a1f
yejin9857 <134900927+yejin9857@users.noreply.github.com>
2023-09-05 04:18:02 +0900

    1차심사_3st

--- src/subject2/script/reference.js
+++ src/subject2/script/reference.js
@@ -1,30 +1,9 @@
 const reference = [
   {
-    author: "lena polishko",
-    title: "",
-    year: "",
-    sourceTitle: "",
-    link: "https://unsplash.com/@ilon_a",
-  },
-  {
-    author: "ash thorp",
-    title: "M O D U L A R - YouTube",
-    year: "2021",
-    sourceTitle: "",
-    link: "https://vimeo.com/486511696",
-  },
-  {
-    author: "ash thorp",
-    title: "FITC Tokyo 2015 Titles",
-    year: "2015",
-    sourceTitle: "",
-    link: "https://vimeo.com/118919656",
-  },
-  {
-    author: "GitHub, Inc.",
-    title: "What is GitHub?",
+    author: "TED-Ed",
+    title: "The 4 greatest threats to the survival of humanity",
     year: "2022",
     sourceTitle: "",
-    link: "https://www.youtube.com/watch?v=pBy1zgt0XPc",
+    link: "https://www.ted.com/talks/ted_ed_the_4_greatest_threats_to_the_survival_of_humanity?language=ko",
   },
 ];
--- src/subject3/script/reference.js
+++ src/subject3/script/reference.js
@@ -1,9 +1,16 @@
 const reference = [
   {
-    author: "",
-    title: "",
-    year: "",
-    sourceTitle: "",
+    author: "신건",
+    title: "악취에 해충까지…떠밀려온 해조류에 몸살",
+    year: "2023",
+    sourceTitle: "KBS뉴스",
+    link: "https://news.kbs.co.kr/news/view.do?ncd=7753118",
+  },
+  {
+    author: "정규열, 우성화",
+    title: "해조류를 이용한 바이오플라스틱 생산 연구동향.",
+    year: "2022",
+    sourceTitle: "한국생물공학회소식지,29(1),31-35.",
     link: "",
   },
 ];
